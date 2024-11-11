d552e20415e4066847e54916193ff086716d5ff9
Francisco Gasca <francisco_gasca@hotmail.com>
2024-11-10 23:18:18 -0600

    Add multiple concepts with its definitions and select one randomly

--- src/hangman-game.js
+++ src/hangman-game.js
@@ -1,3 +1,27 @@
+const concepts = {
+    "Responsabilidad Digital": {
+        "Protección de datos": "Salvaguardar la privacidad y los datos personales de los usuarios en línea",
+        "Transparencia": "Ser claro y honesto en la comunicación y en el uso de datos, evitando prácticas engañosas o fraudulentas",
+        "Seguridad digital": "Implementar medidas que protejan la información sensible de los usuarios contra vulneraciones de seguridad",
+        "Sostenibilidad digital": "Tener en cuenta el impacto ambiental de las actividades tecnológicas, como el consumo de energía"
+    },
+    "Concepto de Ética": {
+        "Ética digital": "Conjunto de principios y normas que guían el comportamiento moral en el uso de la tecnología y el entorno digital",
+        "Responsabilidad digital": "Compromiso de utilizar y compartir tecnologías digitales, en forma segura, ética y responsable",
+        "UNESCO": "Organización cuyo código de ética regula el uso de la Inteligencia Artificial",
+        "Unión Europea para Redes Sociales": "Organización cuyo código de conducta establece pautas para luchar contra la desinformación y el discurso de odio",
+        "Buenas Prácticas en Redes Sociales": "Código que busca regular el comportamiento ético en las plataformas virtuales"
+    },
+    "Ética en la comunicación": {
+        "Identidad digital": "Conjunto de la información sobre un individuo o una organización expuesta en Internet que conforma una descripción de dicha persona en el plano digital",
+        "Seguridad en la Red": "Desafío que puede determinar la estabilidad y el buen funcionamiento del propio sistema",
+        "Acoso virtual": "Una forma de violencia virtual que trasciende los espacios",
+        "Propiedad intelectual": "Al existir una saturación de información, esto complica que los derechos de autor sean respetados",
+        "Difusión de información": "No garantiza ni su calidad ni su veracidad"
+    }
+}
+
+
 let revealedTextElement;
 let textHintElement;
 let correctAnswersElement;
@@ -50,6 +74,22 @@ function load() {
             evaluateLetterClick(element.textContent, evt.target || evt.srcElement);
         })
     });
+}
+
+function getRandomConcept() {
+    const themes = Object.keys(concepts);
+    const randomTheme = themes[Math.floor(Math.random() * themes.length)];
+
+    const themeConcepts = Object.keys(concepts[randomTheme]);
+    const randomConcept = themeConcepts[Math.floor(Math.random() * themeConcepts.length)];
+
+    const definition = concepts[randomTheme][randomConcept];
+
+    return {
+        theme: randomTheme,
+        concept: randomConcept,
+        definition: definition
+    }
 }
 
 function getIndexes(character, text) {
@@ -133,8 +173,9 @@ function createRevealedTextLetters(revealedText) {
 }
 
 function initGame() {
-    text = "Esta es una prueba";
-    textHint = "Esta es una definición de prueba";
+    const result = getRandomConcept();
+    text = result.concept;
+    textHint = result.definition;
 
     const revealedText = text.replace(/\S/g, "_");
 
